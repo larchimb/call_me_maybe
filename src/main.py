840851a68d425a212a2a2b7b020301caae4c1246
larchimb <larchimb@LenovoYogaLuc.localdomain>
2026-09-10 19:42:43 +0200

    parameters found for square root and add, the others need to be more precise

--- src/main.py
+++ src/main.py
@@ -3,12 +3,25 @@ from parser import Parser
 from pydantic import ValidationError
 from models import FunctionsDefinitions
 
+def answer_construction(llm: Small_LLM_Model, sentence: list[int]) -> list[int]:
+    '''The loop for the llm to create the answer'''
+    t_answer = []
+    token_last_word = 0
+    while token_last_word != 151645:
+        logits = llm.get_logits_from_input_ids(sentence)
+        m = max(logits)
+        sentence.append(logits.index(m))
+        t_answer.append(logits.index(m))
+        token_last_word = sentence[-1]
+    return (t_answer)
+
 
 def find_the_function(
     prompt: str,
     functions: list[FunctionsDefinitions],
     llm: Small_LLM_Model
     ) -> FunctionsDefinitions | None:
+    '''Finding the function corresponding to the description'''
     prompt_text = (
      "<|im_start|>system\n"
      "Return a function name corresponding to the user's prompt\n"
@@ -22,15 +35,8 @@ def find_the_function(
      "<think>\n\n</think>\n\n"
     )
     sentence = llm.encode(prompt_text)[0].tolist()
-    t_answer = []
-    token_last_word = 0
-    while token_last_word != 151645:
-        logits = llm.get_logits_from_input_ids(sentence)
-        m = max(logits)
-        sentence.append(logits.index(m))
-        t_answer.append(logits.index(m))
-        token_last_word = sentence[-1]
-    answer = llm.decode(t_answer).strip().strip('"')
+    answer = llm.decode(answer_construction(llm, sentence)).strip().strip('"')
+    print(answer)
     for function in functions:
         if function.name == answer:
             return function
@@ -40,27 +46,25 @@ def find_the_function(
 def find_parameters(
     prompt: str,
     function: FunctionsDefinitions,
-    llm: Small_LLM_Model) -> None:
+    llm: Small_LLM_Model) -> str:
+    '''Finding the parameters corresponding to the function in the prompt'''
     prompt_text = (
          "<|im_start|>system\n"
-         f"Return parameters for {function} from user's prompt\n"
-         "Answer will parse as following : {function.parameters}}"
+         "Extract parameters from a prompt for a function\n"
+         f"You must find {len(function.parameters)} parameter"
+         f"function: \"{function.description}\"\n"
+         "Answer will be parsed as a list of parameters"
          "<|im_end|>\n"
          "<|im_start|>user\n"
-         f"Description: \"{prompt}\"\n"
+         f"Prompt: \"{prompt}\"\n"
          "<|im_end|>\n"
          "<|im_start|>assistant\n"
          "<think>\n\n</think>\n\n"
         )
     sentence = llm.encode(prompt_text)[0].tolist()
-    t_answer = []
-    token_last_word = 0
-    while token_last_word != 151645:
-        logits = llm.get_logits_from_input_ids(sentence)
-        m = max(logits)
-        sentence.append(logits.index(m))
-        t_answer.append(logits.index(m))
-        token_last_word = sentence[-1]
+    answer = llm.decode(answer_construction(llm, sentence)).strip().strip('"')
+    print(answer)
+    return answer
 
 
 def main() -> None:
@@ -80,11 +84,13 @@ def main() -> None:
         dic["name"] = function.name
         dic["parameters"] = find_parameters(item.prompt, function, llm)
         i += 1
-    # print(returned_json)
+        # if i == 1:
+        #     break
+    print(returned_json)
 
 
 if __name__ == "__main__":
-    # try:
-    main()
-    # except (ValidationError, Exception, KeyboardInterrupt) as e:
-        # print(e)
+    try:
+        main()
+    except (ValidationError, Exception, KeyboardInterrupt) as e:
+        print(e)
--- test.py
+++ test.py
@@ -1,7 +1,10 @@
 from llm_sdk import Small_LLM_Model
+import torch
+
+print(torch.cuda.is_available())
 
 
-llm = Small_LLM_Model()
-text = "\""
-test = llm.encode(text)
-print(test)
+# llm = Small_LLM_Model()
+# text = "\""
+# test = llm.encode(text)
+# print(test)
